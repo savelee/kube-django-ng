47c18daf0c67fc586783c5bd26430874ad030b5c
Lee Boonstra <lee@leeboonstra.com>
2019-10-19 16:53:58 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,5 @@ envsubst < cloudbuilder/front-end-deployment.yaml | kubectl apply -f -
 envsubst < cloudbuilder/django-deployment.yaml | kubectl apply -f -
 envsubst < cloudbuilder/chatserver-deployment.yaml | kubectl apply -f -
 
-bold "Starting deployments..."
-kubectl apply -f cloudbuilder/django-deployment.yaml;
-kubectl apply -f cloudbuilder/chatserver-deployment.yaml;
-
 bold "Create services..."
 kubectl apply -f cloudbuilder/services.yaml
